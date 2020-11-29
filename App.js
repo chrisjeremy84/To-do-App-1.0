78e66dca546f59633b403766269d638c22ac9e17
5thfader <53315778+chrisjeremy84@users.noreply.github.com>
2020-11-29 07:49:43 +0100

    Commiting first React_Native Code

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
+  
+  const [name, setName] = useState('')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.TextWelcome}>Welcome</Text>
+      <TextInput 
+      styles={styles.input}
+      placeholder='Input name please'
+      onChangeText={(val) => setName(val)}
+      />
+      <View style ={styles.Btn} >
+      <Text style ={styles.TextStyle} >Connect</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  flex: 1,
+  backgroundColor: '#000000',
+  alignItems: 'center'
   },
+  input: {
+    backgroundColor: '#ffff',
+    fontSize: 14,
+    color: '#fff',
+    borderWidth: 2,
+    borderColor : '#fff',
+    borderRadius: 4,
+    height: 25,
+    width: 200,
+    padding: 8,
+    margin: 10
+  },
+  Btn: {
+    marginTop: 400,
+    marginLeft: 100,
+    marginRight: 100,
+    backgroundColor: '#e35252',
+    borderRadius: 5
+    
+  },
+  TextStyle: {
+    padding: 5,
+    fontSize: 30 ,
+    fontWeight: 'bold',
+    color: '#f2ebeb',
+    textAlign: 'center'
+  },
+  TextWelcome: {
+    padding: 80,
+    fontSize: 40 ,
+    fontWeight: 'bold',
+    color: '#f2ebeb',
+    textAlign: 'center'
+  }
 });
